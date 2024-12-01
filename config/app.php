6bc17f03099bd5d8ca651ddeca56d363b1a11c47
adam <adamfachrezautama@gmail.com>
2024-12-01 14:55:24 +0700

    add fortify and users mvc

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        App\Providers\FortifyServiceProvider::class, // config fortify
         /*
          * Application Service Providers...
          */
